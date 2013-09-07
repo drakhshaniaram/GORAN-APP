52888c4c7c85e5b49c44d6e7eac09cd990b17363
drakhshaniaram <drakhshani.aram@gmail.com>
2013-09-07 07:32:38 -0700

    Some XML JS Corection
    Xml Corrected-Stats(static section) is added Js Corrected
    goran cv's added
    pages dynamicly created by js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // JavaScript Document
 var xml;
+var count;
     $(document).ready(function(){
         $.ajax({
             type: "GET",
@@ -10,6 +11,16 @@ var xml;
 		//loading XML file and parsing to .main div.
 		 function xmlParser(data) {
             xml = data;
+			$(xml).find("barnamayList").each(function() {
+				var list_title=$(this).find("title").text();
+                var list_text=$(this).find("text").text();
+				var list_img_adr=$(this).find("img_adr").text();
+				createPage("00",list_title,"goran",list_text);
+            });
+			$(xml).find("amar").each(function() {
+				var count=$(this).find("count").text();
+				alert()
+            });
             $(xml).find("kandid").each(function () {
                 var id = $(this).find("id").text();
                 var name = $(this).find("name").text();
@@ -28,7 +39,7 @@ var xml;
 				var p = document.createElement("p");
 				
 				img.setAttribute("src","GoranPictures/"+img_adr+".jpg")
-				a.setAttribute("href","#"+id);
+				a.setAttribute("href","#kandid"+id);
 				p.className="jhmare";
 				h3.innerHTML=name;
 				p.innerHTML=id;
@@ -39,8 +50,42 @@ var xml;
 				createPage(id,name,img_adr);
 				$('#listview').listview('refresh'); 
 				}
-			function createPage(id,name,img_adr){
+			function createPage(id,name,img_adr,text){
+				var pageDiv = document.createElement('div');
+				var headerDiv = document.createElement("div");
+				var contentDiv = document.createElement("div");
+				var footerDiv = document.createElement('div');
 				
+				var header_h1 = document.createElement("h1");
+				var content_h2 = document.createElement("h2");
+				var content_img = document.createElement("img");
+				var content_p = document.createElement("p");
+				var footer_h4 = document.createElement("a");
+				
+				pageDiv.setAttribute("data-role","page");
+				pageDiv.setAttribute("id","kandid"+id);
+				headerDiv.setAttribute("data-role","header");
+				contentDiv.setAttribute("data-role","content");
+					content_h2.className="details_title";
+					content_img.className="details_image";
+					content_p.className="details_text";
+				footerDiv.setAttribute("data-role","footer");
+				header_h1.innerHTML="زیانیاری زیاتر";
+				content_h2.innerHTML=name;
+				content_img.src="GoranPictures/"+img_adr+".jpg";
+				content_p.innerHTML=(id=="00")?text:"ژمارە تەلەفۆن و ئیمەیڵ";
+				footer_h4.innerHTML="دەنگ بدە بە گۆڕان لیستی دژی گەندەڵی";
+				
+				headerDiv.appendChild(header_h1);
+				contentDiv.appendChild(content_h2);
+				contentDiv.appendChild(content_img);
+				contentDiv.appendChild(content_p);
+				footerDiv.appendChild(footer_h4);
+				pageDiv.appendChild(headerDiv);
+				pageDiv.appendChild(contentDiv);
+				pageDiv.appendChild(contentDiv);
+				
+				document.getElementById("body").appendChild(pageDiv);
 				}
     });
         

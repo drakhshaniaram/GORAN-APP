a1ca99ee283ea051ac4e70367abadb045f0bbeb5
drakhshaniaram <drakhshani.aram@gmail.com>
2013-09-07 21:45:20 -0700

    MakeSimpleList Without image

--- script.js
+++ script.js
@@ -7,20 +7,44 @@ var xml;
             dataType: "xml",
             success: xmlParser
         });
-    });
-        //loading XML file and parsing to .main div.
-        function xmlParser(data) {
+		//loading XML file and parsing to .main div.
+		 function xmlParser(data) {
             xml = data;
             $('#load').fadeOut();
             $(xml).find("kandid").each(function () {
                 var id = $(this).find("id").text();
                 var name = $(this).find("name").text();
-                var nasyawbe = $(this).find("nasyawbe").text();
-                var imgaddrr = $(this).find("imgaddrr").text();
-                var slogan = $(this).find("slogan").text();
-                var details = $(this).find("details").text();
-                var listcode = $(this).find("listcode").text();
-				$("#kandidList").append('<li> '+ name +'</li>');
-				$('#listview').listview('refresh'); 
+                var img_adr = $(this).find("img_adr").text();
+                var programs = $(this).find("programs").text();
+				var contact = $(this).find("contact").text();
+				createList(id,name,img_adr,programs,contact);
             });
         }
+		function createList(id,name,img_adr,programs,contact){
+			
+				var listview=document.getElementById("listview");
+				
+				var listItem = document.createElement('li');
+				var a = document.createElement("a");
+				var img = document.createElement("img");
+				var h3 = document.createElement("h3");
+				var p = document.createElement("p");
+				
+				img.setAttribute("src","images/"+img_adr+".jpg");
+				a.setAttribute("href","#"+id);
+				p.className="jhmare";
+				h3.innerHTML=name;
+				p.innerHTML=id;
+        		
+				a.appendChild(img);a.appendChild(h3);a.appendChild(p);
+				listItem.appendChild(a);
+                listview.appendChild(listItem);
+				createPage(id,name,img_adr,programs,contact);
+				$('#listview').listview('refresh'); 
+				}
+			function createPage(id,name,img_adr,programs,contact){
+				
+				}
+    });
+        
+       

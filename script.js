54298828658c1cc55cf391ac1a33758c0d0c9b25
drakhshaniaram <drakhshani.aram@gmail.com>
2013-09-07 21:08:35 -0700

    Get Simple XML

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // JavaScript Document
+var xml;
+    $(document).ready(function(){
+        $.ajax({
+            type: "GET",
+            url: "data_src.xml",
+            dataType: "xml",
+            success: xmlParser
+        });
+    });
+        //loading XML file and parsing to .main div.
+        function xmlParser(data) {
+            xml = data;
+            $('#load').fadeOut();
+            $(xml).find("kandid").each(function () {
+                var id = $(this).find("id").text();
+                var name = $(this).find("name").text();
+                var nasyawbe = $(this).find("nasyawbe").text();
+                var imgaddrr = $(this).find("imgaddrr").text();
+                var slogan = $(this).find("slogan").text();
+                var details = $(this).find("details").text();
+                var listcode = $(this).find("listcode").text();
+				$("#kandidList").append('<li> '+ name +'</li>');
+				$('#listview').listview('refresh'); 
+            });
+        }
